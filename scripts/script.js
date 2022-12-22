c862b93f4c8e8f8d611fe19c2ecb276e7bd88ae7
Marco <marco.kosters@hva.nl>
2022-12-22 20:41:55 +0100

    java in grid

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,4 @@
 function scrollButton() {
-    var kennisclip = document.getElementById("myDIV");
-    kennisclip.classList.toggle("show");
-
-    var kennisclip = document.getElementById("myDIV2");
-    kennisclip.classList.toggle("show2");
-
-    var kennisclip = document.getElementById("myDIV3");
-    kennisclip.classList.toggle("show3");
-
-    var kennisclip = document.getElementById("myDIV4");
-    kennisclip.classList.toggle("show4");
-
-    var uitleg = document.getElementById("text1");
-    uitleg.classList.toggle("showText1");
-
-    var kennisclipText = document.getElementById("text2");
-    kennisclipText.classList.toggle("showText2");
-
-    var kennisclipText = document.getElementById("text3");
-    kennisclipText.classList.toggle("showText3");
-
-    var kennisclipText = document.getElementById("text4");
-    kennisclipText.classList.toggle("showText4");
-
-    var kennisclipText = document.getElementById("text5");
-    kennisclipText.classList.toggle("showText5");
-
-    var button = document.getElementById("button");
-    button.classList.toggle("button-reversed");
+    var kennisclips = document.getElementById("hide");
+    kennisclips.classList.add("show");
   }
